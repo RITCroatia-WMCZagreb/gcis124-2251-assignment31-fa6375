8f2a0efa04c8a9a6225b313f6d06173adcf5b516
Faraj07 <fa6375@g.rit.edu>
2025-09-21 20:41:49 +0200

    Pushing the H/W

--- more_math.py
+++ more_math.py
@@ -1,13 +1,48 @@
 '''
-@ASSESSME.USERID: userID
-@ASSESSME.AUTHOR: author, list of authors
+@ASSESSME.USERID: fa6375
+@ASSESSME.AUTHOR: Faraj Aliyev
 @ASSESSME.DESCRIPTION: Assignment 3.1
 @ASSESSME.ANALYZE: YES
 '''
 
+import math
+
+
+def fact(x):
+    if x > 0: 
+        return math.factorial(int(x))
+    return 0
+
+def root(y):
+    if y > 0:
+      return math.sqrt(float(y))
+    return 0
+
+
+def trunk(z):
+    return math.trunc(z)
+    
+    
+"""
+  Expected:10 factorial = 3628800
+  The square root of 9.0 = 3.0
+  10.5 truncated = 10
+  Returned:Factorial is:  3628800
+  The square root of 9.0 is 3.0
+  truncated 10
+  """
+
 
 def main():
-   pass
+   a1 = int(input("Enter a numeric value: "))
+   print(a1, "factorial","=",fact(a1))
+
+
+   a2 = float(input("Enter a numeric value: "))
+   print("The square root of",a2, "=", root(a2))
+
+   a3 = float(input("Enter a numeric value: "))
+   print(a3,"truncated","=",trunk(a3))
 
 
 if __name__ == "__main__":
